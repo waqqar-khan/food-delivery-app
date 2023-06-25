a9af5eeeef797ad9bdd4c2dbe243d7837d315b9d
Wick <waqqarkhan248@gmail.com>
2023-06-25 23:08:20 +0530

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,23 @@
 import React from "react";
+import "../styles/Header.scss";
 
 const Footer = () => {
     return (
         <>
-        <div>This is Footer</div>
+        <div className="header-wrapper">
+            <div className="header-logo">
+                <img src="https://images.freecreatives.com/wp-content/uploads/2016/03/Restaurant-logos.jpg" alt="Restaurent Logo" />
+                <span>Footer</span>
+            </div>
+            <div className="header-ryt-side-items">
+                <ul>
+                    <li>HOME</li> 
+                    <li>ABOUT</li>
+                    <li>CONTACT</li>
+                    <li>CART</li>
+                </ul>
+            </div>
+        </div>
         </>
     )
 };
